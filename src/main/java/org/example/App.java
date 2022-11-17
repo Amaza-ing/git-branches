b022e1dbfea706dfceab33996a40201b60056874
Adrián Maza <adri_mv1@hotmail.com>
2022-11-17 19:38:58 +0100

    branch1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Modificado en master");
+        System.out.println("Modificado en branch1");
+    }
+
+    private void change() {
+        System.out.println("branch1");
     }
 }
